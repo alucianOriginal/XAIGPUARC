a31db19c8045a1bfa4fa3f3aeb825325be412d1c
alucian <saschapurner@gmail.com>
2025-12-12 21:50:48 +0100

    Add F16 mode and model specifications to script

--- XAIGPUARC.sh
+++ XAIGPUARC.sh
@@ -33,17 +33,29 @@
 #-BF16 Models not recommend for Alechmist
 
 
+#F16 Mode Only:
+
+#6GB GPU A730m/A380/A310
 
 #Qwen2.5-VL-3B-Instruct-f16-q4_k.gguf            2.1     GB
+
+#8GB GPU A750LE
+
 #Qwen2.5-7B-Instruct-f16-q4_k.ggu                5.7     GB
 #Qwen2.5-VL-3B-Instruct-f16.gguf                 5.8     GB
 #llama3bthinkingonly5B.f16.gguf                  6.0     GB
+
+#10-12GB GPU-iGPU-Xe-LPG/A730m/B570/B580/PRO
+
 #Qwen3-Embedding-4B-f16.gguf                     7.5     GB
 #Qwen3-4B-f16.gguf                               7.5     GB
 #Nemotron-Mini-4B-Instruct-f16.gguf              7.8     GB
 #Minitron-4B-Base.FP16-.gguf                     7.8     GB
 #DiffuCoder-7B-cpGRPO-f16_q8_0.gguf              10.5    GB
 #KernelLLM-f16_q8_0.gguf                         11.1    GB
+
+#16GB GPU A770LE
+
 #Nemotron-Orchestrator-8B-f16_q8_0.gguf          11.4    GB
 #Orchestrator-8B-f16_q8_0.gguf                   11.4    GB
 #ggml-model-f16.gguf                             12.6    GB
@@ -51,21 +63,31 @@
 #Mistral-7B-Instruct-v0.3.fp16.gguf              13.5    GB
 #OpenReasoning-Nemotron-7B-F16.gguf              14.1    GB
 #MathTutor-7B-H_v0.0.1.f16.gguf                  14.2    GB
+
+#16+8GB Dual GPU A770LE/A750LE
 #NVIDIA-Nemotron-Nano-12B-v2-F16.gguf            22.9    GB
 
-#NOT F16! MODE but also well Tested:
 
+
+#NOT F16! MODE
+#6GB GPU A730m/A380/A310
 #openhermes-2.5-mistral-7b.Q4_K_M.gguf           4.1     GB 
 #mistral-7b-instruct-v0.2.Q4_K_M.gguf            4.1     GB
 #OpenMath-Mistral-7B-v0.1-hf_Q6_K.gguf           5.5     GB
+
+#8GB GPU A750LE
 #NVIDIA-Nemotron-Nano-12B-v2-IQ4_NL.gguf         6.6     GB
 #wizardcoder-python-7b-v1.0.Q8_0.gguf            6.7     GB
 #sauerkrautlm-7b-v1.Q8_0.gguf                    6.7     GB
+
+#10-12GB GPU-iGPU-Xe-LPG/A730m/B570/B580/PRO
 #Qwen3-16B-A3B-IQ4_NL.gguf                       8.5     GB
 #Qwen3-30B-A3B-UD-IQ2_XXS.gguf                   9.7     GB
 #solar-10.7b-instruct-v1.0-uncensored.Q8_0.gguf  10.6    GB
 #gpt-oss-20b-claude-4-distill.MXFP4_MOE.gguf     11.3    GB
 #gpt-oss-20b-mxfp4.gguf                          11.3    GB
+
+#16GB GPU A770LE
 #wizardcoder-python-13b-v1.0.Q8_0.gguf           12.9    GB
 #Deepseek-Coder-V2-Lite-13B-
 #Instruct-sft-s1K.i1-Q6_K.gguf                   13.1    GB
