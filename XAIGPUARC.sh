ffd56e62d9aa7ea0d8f49fa7ce57622ed892bb51
alucian <saschapurner@gmail.com>
2025-12-13 18:36:49 +0100

    Update XAIGPUARC.sh

--- XAIGPUARC.sh
+++ XAIGPUARC.sh
@@ -101,7 +101,7 @@
 #OpenMath-Mistral-7B-v0.1-hf_Q6_K.gguf           5.5  GB FAST CTX-NPG 8k A770LE: 1233.9 Pt/s 14.4 Gt/s 145w 2.4Ghz - CPU Low Oldscool
 #NVIDIA-Nemotron-Nano-12B-v2-IQ4_NL.gguf         6.6  GB SLOW
 #wizardcoder-python-7b-v1.0.Q8_0.gguf            6.7  GB SLOW
-#sauerkrautlm-7b-v1.Q8_0.gguf                    6.7  GB FAST
+#sauerkrautlm-7b-v1.Q8_0.gguf                    6.7  GB FAST CTX-NPG 8k A770LE: 1364.6 Pt/s 12.1 Gt/s 142w 2.4Ghz - CPU Low Oldscool
 
 #10-12GB+ GPU-iGPU-Xe-LPG/A730m/B570/B580/PRO
 #Qwen3-16B-A3B-IQ4_NL.gguf                       8.5  GB FAST
